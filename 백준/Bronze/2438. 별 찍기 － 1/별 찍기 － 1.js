63eb903aabe9ba00bbfb4d9341eee0f3b1c236ec
현준 <121845820+hyunjun9788@users.noreply.github.com>
2024-08-19 20:48:26 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 88 ms, Memory: 9480 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -4,6 +4,9 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 let a = Number(input[0])
 let star = ''
 for(let i=1;i<=a;i++){
-    star+='*'
-    console.log(star)
+    for(let j=1;j<=i;j++){
+        star+='*'
+    }
+    star+='\n'
 }
+  console.log(star)
