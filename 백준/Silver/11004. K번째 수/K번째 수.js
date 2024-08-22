6632c607f173c3d2ce9742bbae72c627dbdf50eb
현준 <121845820+hyunjun9788@users.noreply.github.com>
2024-08-22 18:51:10 +0900

    [Silver V] Title: K번째 수, Time: 4128 ms, Memory: 562840 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.js
+++ 백준/Silver/11004. K번째 수/K번째 수.js
@@ -1,8 +1,12 @@
 let fs = require('fs')
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let count = Number(input[0].split(' ')[0])
-let pick = Number(input[0].split(' ')[1])
+let [n, k] = input[0].split(' ').map(Number)
+let arr = input[1].split(' ').map(Number)
 
-let arr = input[1].split(' ').map(Number).sort((a,b)=>a-b)
-console.log(arr[pick-1])
+
+arr.sort(function(a,b){
+    return a-b
+})
+
+console.log(arr[k-1])
