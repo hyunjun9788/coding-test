ec78d7b0429b98da9dd8bed6aa26cb8e7b659902
현준 <121845820+hyunjun9788@users.noreply.github.com>
2024-08-25 00:08:42 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 656 ms, Memory: 164408 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -2,39 +2,13 @@ let fs = require('fs')
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 
-let n = Number(input[0])
-let arr = input[1].split(' ').map(Number)
-let m = Number(input[2])
-let query = input[3].split(' ').map(Number)
-
-arr.sort((a,b) => a-b)
-
-function lowerBound(arr,target,start,end){
-    while(start<end){
-        let mid = parseInt((start+end)/2)
-        if(arr[mid]>=target) end=mid;
-        else start = mid + 1
-    }
-    return end
-}
-function upperBound(arr,target,start,end){
-    while(start<end){
-        let mid = parseInt((start+end)/2)
-        if(arr[mid]>target) end=mid;
-        else start = mid + 1
-    }
-    return end
-}
-
-function countByRange(arr,leftValue,rightValue){
-    let rightIndex = upperBound(arr, rightValue,0,arr.length);
-    let leftIndex = lowerBound(arr,leftValue,0,arr.length);
-    return rightIndex - leftIndex
-}
-
-let answer = '' 
-for(let i=0; i<m;i++){
-    let cnt = countByRange(arr,query[i],query[i])
-    answer += cnt + ' '
-}
-console.log(answer)
+let N = input[1].split(' ').map(Number);
+let M = input[3].split(' ').map(Number);
+let map = new Map();
+N.forEach(el => {
+  if(map.has(el)) map.set(el,map.get(el)+1);
+  else map.set(el,1);
+});
+let answer = [];
+M.forEach(el => answer.push(map.get(el)||0));
+console.log(answer.join(' '));
