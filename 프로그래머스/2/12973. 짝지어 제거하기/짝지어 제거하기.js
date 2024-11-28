3fe04a0e27ee46e8303f5e5dbe2905e7af2381f2
현준 <121845820+hyunjun9788@users.noreply.github.com>
2024-11-28 12:28:40 +0900

    [level 2] Title: 짝지어 제거하기, Time: 44.23 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,13 +1,20 @@
-function solution(s) {
-    const stack = [];
+function solution(s)
+{
+    const stack = []
+    
+    for(let i=0;i<=s.length;i++){
 
-    for (let i = 0; i < s.length; i++) {
-        if (stack.length > 0 && stack[stack.length - 1] === s[i]) {
-            stack.pop();
-        } else {
-            stack.push(s[i]);
+        const top = stack[stack.length-1]
+        if(s[i] === top){
+            stack.pop()
+        }else{
+            stack.push(s[i])
         }
+        
     }
-
-    return stack.length === 0 ? 1 : 0;
+    if(!stack.length){
+        return 1
+    }else{
+        return 0
+    }
 }
