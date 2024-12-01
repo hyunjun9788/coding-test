68f34e1ecb277d444e067ed497e878121d582b76
현준 <121845820+hyunjun9788@users.noreply.github.com>
2024-12-02 00:29:03 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.73 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,26 +1,26 @@
 function solution(arr1, arr2) {
     
-    const r1 = arr1.length
-    const c1 = arr1[0].length
+    const row1 = arr1.length
+    const col1 = arr1[0].length
     
-    const r2 = arr2.length
-    const c2 = arr2[0].length
+    const row2 = arr2.length
+    const col2 = arr2[0].length
     
-    
-    const ret =[]
-    
-    for(let i=0;i<r1;i++){
-        ret.push(new Array(c2).fill(0))
+    const arr = []
+    for(let i=0;i<row1;i++){
+        arr[i]=[]
+        for(let j=0;j<col2;j++){
+            arr[i][j] =0
+        }
     }
     
-    
-    for(let i=0;i<r1;i++){
-        for(let j=0;j<c2;j++){
-              for(let k=0;k<c1;k++){
-                ret[i][j] += arr1[i][k] * arr2[k][j]
+    for(let i=0;i<row1;i++){
+        for(let j=0;j<col2;j++){
+            for(let k=0;k<col1;k++){
+                arr[i][j] += arr1[i][k] * arr2[k][j] 
+            }
         }
-        }
-      
     }
-    return ret;
+    
+    return arr
 }
