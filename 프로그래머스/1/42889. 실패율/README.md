15149478fac3c303f6c7ec8f883f5e3dcfb8c95e
현준 <121845820+hyunjun9788@users.noreply.github.com>
2024-12-02 12:27:39 +0900

    [level 1] Title: 실패율, Time: 90.36 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
 
 ### 성능 요약
 
-메모리: 40.4 MB, 시간: 375.75 ms
+메모리: 41.5 MB, 시간: 90.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 24일 22:34:42
+2024년 12월 02일 12:27:37
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,22 +1,31 @@
 function solution(N, stages) {
-    const failPercent = []
+    let member = stages.length
+    const arr= []
     for(let i=1;i<=N;i++){
-        let cnt = 0
-        let totalUser = 0
+        let noPass = 0
         for(let j=0;j<stages.length;j++){
-            if(i===stages[j]){
-                cnt++
-            }
-            if(stages[j]>=i){
-                totalUser++
+            if(stages[j] === i){
+                noPass++
             }
         }
+        console.log(noPass)
+        console.log('member',member)
+         arr.push(noPass/member)
+        member-=noPass
         
-        failPercent.push(cnt/totalUser)
     }
+    const result = []
+    console.log(arr)
+    for(const item of arr.entries()){
+        result.push(item)
+        
+}
+   result.sort((a,b)=>b[1]-a[1])
+    console.log(result)
     
-    const result = failPercent.map((value, index) => ({ value, index }))
-    .sort((a, b) => b.value - a.value)
-    .map(item => item.index+1);   
-    return result
+    const index = []
+    for(let i=0;i<result.length;i++){
+        index.push(result[i][0]+1)
+    }
+    return index
 }
