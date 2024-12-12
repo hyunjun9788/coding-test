be15c46f4b604809894f77b1f65221c07c2181dc
현준 <121845820+hyunjun9788@users.noreply.github.com>
2024-12-12 16:37:19 +0900

    [Silver I] Title: 트리 순회, Time: 92 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.js
@@ -0,0 +1,50 @@
+(function main() {
+    let isLocal = false;
+    let fs = require("fs");
+    let filePath = isLocal ? "test.txt" : "/dev/stdin";
+    let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+    const N = Number(input.shift())
+    let result = ''
+
+    const tree = {}
+
+    for (let i = 0; i < N; i++) {
+        const [node, left, right] = input[i].split(' ')
+        tree[node] = [left, right]
+    }
+    function preorder(node) {
+        if (node === '.') {
+            return
+        }
+        const [left, right] = tree[node]
+        result += node
+        preorder(left)
+        preorder(right)
+    }
+
+    function inorder(node) {
+        if (node === ".") return;
+        const [left, right] = tree[node];
+        inorder(left);
+        result += node;
+        inorder(right);
+    }
+
+    function postorder(node) {
+        if (node === ".") return;
+        const [left, right] = tree[node];
+        postorder(left);
+        postorder(right);
+        result += node;
+    }
+
+    preorder("A");
+    result += "\n";
+    inorder("A");
+    result += "\n";
+    postorder("A");
+
+    console.log(result);
+}
+)();
