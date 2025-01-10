1eafb4fa59968d88ecfe8721c27f034a5f9895f5
현준 <121845820+hyunjun9788@users.noreply.github.com>
2025-01-10 13:51:56 +0900

    [Bronze II] Title: 거스름돈, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.js
+++ 백준/Bronze/5585. 거스름돈/거스름돈.js
@@ -4,40 +4,13 @@
   const filePath = isLocal ? "t.txt" : "/dev/stdin";
   const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-  let cost = Number(input[0]);
-  let payment = 1000;
-
-  const change = payment - cost;
-
-  let count = 0;
-  let remain = change;
-
-  while (remain > 0) {
-    if (change >= 500) {
-      count += Math.floor(remain / 500);
-      remain = remain % 500;
-    }
-    if (remain >= 100 && remain < 500) {
-      count += Math.floor(remain / 100);
-      remain = remain % 100;
-    }
-    if (remain >= 50 && remain < 100) {
-      count += Math.floor(remain / 50);
-      remain = remain % 50;
-    }
-    if (remain >= 10 && remain < 50) {
-      count += Math.floor(remain / 10);
-      remain = remain % 10;
-    }
-    if (remain >= 5 && remain < 10) {
-      count += Math.floor(remain / 5);
-      remain = remain % 5;
-    }
-    if (remain >= 1 && remain < 5) {
-      count += Math.floor(remain / 1);
-      remain = remain % 1;
-    }
-    break;
+  const coins = [500, 100, 50, 10, 5, 1];
+  let cnt = 0;
+  let money = 1000 - Number(input[0]);
+  for (let coin of coins) {
+    cnt += Math.floor(money / coin);
+    money = money % coin;
   }
-  console.log(count);
+
+  console.log(cnt);
 })();
