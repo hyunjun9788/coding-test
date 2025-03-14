6cbf68dd96486c513094d341aa7d6bd303ae17f2
현준 <121845820+hyunjun9788@users.noreply.github.com>
2025-03-15 00:43:45 +0900

    [Silver II] Title: 쇠막대기, Time: 188 ms, Memory: 11260 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.js
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.js
@@ -4,25 +4,23 @@
   let filePath = isLocal ? "t.txt" : "/dev/stdin";
   let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-  const parentheses = input[0];
+  const arr = input[0];
 
   const stack = [];
-  let answer = 0;
-
-  for (let i = 0; i < parentheses.length; i++) {
-    if (parentheses[i] === "(") {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "(") {
       stack.push(i);
     } else {
-      if (stack.length > 0) {
-        if (stack[stack.length - 1] === i - 1) {
-          stack.pop();
-          answer += stack.length;
-        } else {
-          stack.pop();
-          answer += 1;
-        }
+      if (stack[stack.length - 1] === i - 1) {
+        stack.pop();
+        result += stack.length;
+      } else {
+        stack.pop();
+        result += 1;
       }
     }
   }
-  console.log(answer);
+
+  console.log(result);
 })();
