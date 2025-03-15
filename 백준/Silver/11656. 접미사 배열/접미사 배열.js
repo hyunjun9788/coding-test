0f958580d132c5ef13632b75cce646befc89a758
현준 <121845820+hyunjun9788@users.noreply.github.com>
2025-03-15 18:38:39 +0900

    [Silver IV] Title: 접미사 배열, Time: 120 ms, Memory: 13180 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.js
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.js
@@ -1,22 +1,15 @@
 (function main() {
-    let isLocal = false;
-    let fs = require("fs");
-    let filePath = isLocal ? "t.txt" : "/dev/stdin";
-    let input = fs.readFileSync(filePath).toString().trim().split("\n");
-    let str = input[0].trim().split('')
-    let str2 = [...str]
-    let result = []
-    result.push(str2)
-    for (let i = 0; i < input[0].length; i++) {
-        if (str.length >= 2) {
-            str.shift()
-            let str3 = [...str]
-            result.push(str3)
-        }
+  let isLocal = false;
+  let fs = require("fs");
+  let filePath = isLocal ? "t.txt" : "/dev/stdin";
+  let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-    }
+  const s = input[0].split("");
+  const result = [];
+  for (let i = 0; i < s.length; i++) {
+    const sliceS = s.slice(i, s.length);
+    result.push(sliceS.join(""));
+  }
 
-
-    const sortedStrings = result.map(v => v.join('')).sort()
-    console.log(sortedStrings.join('\n'))
+  console.log(result.sort().join("\n"));
 })();
