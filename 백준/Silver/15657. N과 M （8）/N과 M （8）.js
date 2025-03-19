fcdabe852940981a2c166d722709447c8b5a9500
현준 <121845820+hyunjun9788@users.noreply.github.com>
2025-03-19 22:40:27 +0900

    [Silver III] Title: N과 M (8), Time: 172 ms, Memory: 16444 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.js
@@ -0,0 +1,34 @@
+(function main() {
+  let isLocal = false;
+  let fs = require("fs");
+  let filePath = isLocal ? "t.txt" : "/dev/stdin";
+  let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+  const [n, m] = input[0].split(" ").map(Number);
+
+  const arr = input[1].split(" ").map(Number);
+
+  arr.sort((a, b) => a - b);
+
+  const result = [];
+  let answer = "";
+
+  function recur(index) {
+    if (result.length === m) {
+      for (let i = 0; i < result.length; i++) {
+        answer += result[i] + " ";
+      }
+      answer += "\n";
+      return;
+    }
+
+    for (let i = index; i < arr.length; i++) {
+      result.push(arr[i]);
+      recur(i);
+      result.pop();
+    }
+  }
+
+  recur(0);
+  console.log(answer);
+})();
