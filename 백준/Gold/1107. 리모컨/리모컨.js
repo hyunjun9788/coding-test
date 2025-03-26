53bd2a59f9049a5725ba01eb906d73625f9a0ce0
현준 <121845820+hyunjun9788@users.noreply.github.com>
2025-03-27 00:05:31 +0900

    [Gold V] Title: 리모컨, Time: 216 ms, Memory: 10248 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.js
+++ 백준/Gold/1107. 리모컨/리모컨.js
@@ -5,19 +5,20 @@
   let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
   const want = Number(input[0]);
-  const check = Array.from({ length: 10 }, () => true);
-  const m = Number(input[1]);
-  if (m > 0) {
-    const noButton = input[2].split(" ").map(Number);
 
-    for (let i = 0; i < m; i++) {
-      check[noButton[i]] = false;
+  const disabledCheck = Array.from({ length: 10 }, () => false);
+  const disabledBtnCnt = Number(input[1]);
+
+  if (disabledBtnCnt >= 1) {
+    const disabledBtns = input[2].split(" ").map(Number);
+    for (let i = 0; i < disabledBtns.length; i++) {
+      disabledCheck[disabledBtns[i]] = true;
     }
   }
 
   function possible(value) {
     if (value === 0) {
-      if (!check[0]) {
+      if (disabledCheck[0]) {
         return -1;
       }
       return 1;
@@ -25,24 +26,24 @@
 
     let count = 0;
     while (value > 0) {
-      if (!check[value % 10]) {
+      if (disabledCheck[value % 10]) {
         return -1;
       }
       value = Math.floor(value / 10);
-      count += 1;
+      count++;
     }
     return count;
   }
 
   let min = Math.abs(want - 100);
+
   for (let i = 0; i <= 1000000; i++) {
     const count = possible(i);
     if (count === -1) {
       continue;
-    } else {
-      const diff = Math.abs(i - want);
-      min = Math.min(min, diff + count);
     }
+    min = Math.min(min, count + Math.abs(want - i));
   }
+
   console.log(min);
 })();
