cba4211111afe202e2b676605e7a2cd3b945ecfc
현준 <121845820+hyunjun9788@users.noreply.github.com>
2025-03-27 16:40:16 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 568 ms, Memory: 87344 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.js
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.js
@@ -1,38 +1,36 @@
 (function main() {
-    let isLocal = false;
-    let fs = require("fs");
-    let filePath = isLocal ? "test.txt" : "/dev/stdin";
-    let input = fs.readFileSync(filePath).toString().trim().split("\n");
+  let isLocal = false;
+  let fs = require("fs");
+  let filePath = isLocal ? "t.txt" : "/dev/stdin";
+  let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
+  const [n, m] = input[0].split(" ").map(Number);
 
-    const [n, m] = input[0].split(' ').map(Number)
+  const arr = Array.from({ length: 1 }, () => Array(n + 1).fill(0));
 
-    let graph = []
-    for (let i = 0; i < n; i++) {
-        graph[i] = input[i + 1].split(' ').map(Number)
+  const sumArr = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+
+  for (let i = 1; i <= n; i++) {
+    const row = [0];
+    row.push(...input[i].split(" ").map(Number));
+    arr.push(row);
+  }
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      sumArr[i][j] = sumArr[i - 1][j] + sumArr[i][j - 1] - sumArr[i - 1][j - 1] + arr[i][j];
     }
+  }
+  let answer = "";
 
-    let answer = []
+  for (let i = n + 1; i < input.length; i++) {
+    let sum = 0;
+    const [x1, y1, x2, y2] = input[i].split(" ").map(Number);
 
-    let prefix = Array.from({ length: n + 1 }, () => Array.from({ length: n + 1 }, () => 0))
+    sum += sumArr[x2][y2] - sumArr[x1 - 1][y2] - sumArr[x2][y1 - 1] + sumArr[x1 - 1][y1 - 1];
 
+    answer += sum + "\n";
+  }
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            prefix[i + 1][j + 1] = prefix[i + 1][j] + prefix[i][j + 1] + graph[i][j] - prefix[i][j]
-        }
-    }
-
-
-    for (let i = n + 1; i <= n + m; i++) {
-        const [x1, y1, x2, y2] = input[i].split(' ').map(Number)
-
-        let result = prefix[x2][y2] - prefix[x2][y1 - 1] - prefix[x1 - 1][y2] + prefix[x1 - 1][y1 - 1]
-        answer.push(result)
-    }
-
-    console.log(answer.join('\n'))
-}
-
-
-)();
+  console.log(answer);
+})();
