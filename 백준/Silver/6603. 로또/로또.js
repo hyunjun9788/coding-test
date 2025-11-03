52a64f3293a974de48eca43bc9952eb2d4ee0393
현준 <121845820+hyunjun9788@users.noreply.github.com>
2025-11-03 23:53:21 +0900

    [Silver II] Title: 로또, Time: 228 ms, Memory: 11960 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.js
+++ 백준/Silver/6603. 로또/로또.js
@@ -1,35 +1,35 @@
 (function main() {
-    const fs = require("fs");
-    const isLocal = false;
-    const filePath = isLocal ? "t.txt" : "/dev/stdin";
-    const input = fs.readFileSync(filePath).toString().trim().split('\n');
+  let isLocal = false;
+  let fs = require("fs");
+  let filePath = isLocal ? "test.txt" : "/dev/stdin";
+  let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
+  for (let i = 0; i < input.length - 1; i++) {
+    const inputArr = input[i].split(" ").map(Number);
 
-    for (let i = 0; i < input.length - 1; i++) {
-        const inputArr = input[i].split(' ').map(Number)
-        const k = inputArr[0]
-        const arr = []
+    const k = inputArr[0];
+    const arr = [];
 
-        const result = []
+    const result = [];
 
-        for (let j = 1; j <= k; j++) {
-            arr.push(inputArr[j])
-        }
-        function combination(index, level) {
-
-            if (level === 6) {
-                console.log(result.join(' '))
-                return
-            }
-            for (let k = index; k < arr.length; k++) {
-                result.push(arr[k])
-                combination(k + 1, level + 1)
-                result.pop()
-            }
-        }
-
-        combination(0, 0)
-        console.log('')
-
+    for (let j = 1; j < inputArr.length; j++) {
+      arr.push(inputArr[j]);
     }
+
+    function combination(index, level) {
+      if (level === 6) {
+        console.log(result.join(" "));
+        return;
+      }
+
+      for (let k = index; k < arr.length; k++) {
+        result.push(arr[k]);
+        combination(k + 1, level + 1);
+        result.pop();
+      }
+    }
+
+    combination(0, 0);
+    console.log("");
+  }
 })();
